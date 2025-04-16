cd59ba7bf7bf16617941547664274680261b4a08
christiano02 <christianonascimento02@hotmail.com>
2025-04-15 23:24:43 -0300

    Configurando modelos User, Post e Enum

--- src/main/java/com/descomplica/FrameBlog/models/Post.java
+++ src/main/java/com/descomplica/FrameBlog/models/Post.java
@@ -1,4 +1,29 @@
 package com.descomplica.FrameBlog.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Post")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long postId;
+
+    private String title;
+
+    private String content;
+
+    private Date date;
+
+    @ManyToOne
+    private User user;
 }
--- src/main/java/com/descomplica/FrameBlog/models/User.java
+++ src/main/java/com/descomplica/FrameBlog/models/User.java
@@ -1,4 +1,24 @@
 package com.descomplica.FrameBlog.models;
 
+import com.descomplica.FrameBlog.enums.RoleEnum;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "User")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userId;
+    private String name;
+    private String email;
+    private String password;
+    private RoleEnum role;
+
 }
