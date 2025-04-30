5f731ae61d46e739010299af6ece7fc7f2f3bd1a
christiano02 <christianonascimento02@hotmail.com>
2025-04-29 22:41:13 -0300

    Removendo o lombok para evitar erros futuros

--- src/main/java/com/descomplica/FrameBlog/models/Post.java
+++ src/main/java/com/descomplica/FrameBlog/models/Post.java
@@ -1,17 +1,10 @@
 package com.descomplica.FrameBlog.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
 @Entity
 @Table(name = "Post")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Post {
 
     @Id
@@ -26,4 +19,55 @@ public class Post {
 
     @ManyToOne
     private User user;
+
+    public Post() {
+    }
+
+    public Post(final User user, final String title, final long postId, final Date date, final String content) {
+        this.user = user;
+        this.title = title;
+        this.postId = postId;
+        this.date = date;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
