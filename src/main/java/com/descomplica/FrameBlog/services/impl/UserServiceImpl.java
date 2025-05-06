7c88635f6a0b7298b94eca15e74ee6a24418edc6
Christiano <christianonascimento02@hotmail.com>
2025-05-06 13:30:06 -0300

    Implementando a interface UserService na CLASSE UserServiceimpl

--- src/main/java/com/descomplica/FrameBlog/services/impl/UserServiceImpl.java
+++ src/main/java/com/descomplica/FrameBlog/services/impl/UserServiceImpl.java
@@ -1,7 +1,35 @@
 package com.descomplica.FrameBlog.services.impl;
 
+import com.descomplica.FrameBlog.models.User;
+import com.descomplica.FrameBlog.services.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public User get(long id) {
+        return null;
+    }
+
+    @Override
+    public User update(long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
 }
